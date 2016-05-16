dbe420b1998acd2afd6cbb7aa2bdd68efb15975c
Andrej Sakal <andi.sakal15@gmail.com>
2016-05-16 21:30:43 +0200

    Renamed classes

--- Hand_in_Hand/src/main/java/htl/leonding/hand_in_hand/HandInHand.java
+++ Hand_in_Hand/src/main/java/htl/leonding/hand_in_hand/HandInHand.java
@@ -1,4 +1,4 @@
-package com.htlleonding.hand_in_hand;
+package htl.leonding.hand_in_hand;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -11,14 +11,14 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 
-public class MainApp extends Application {    
+public class HandInHand extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
+        scene.getStylesheets().add("/styles/ButonAnimation.css");
         stage.getIcons().add(new Image(getClass().getResource("/ico.jpg").toExternalForm()));
         stage.setTitle("Hand in Hand");
         stage.setScene(scene);
