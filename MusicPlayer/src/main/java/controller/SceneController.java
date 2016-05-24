fc6bb309e9f6c6f5f7b7221a0384031f7fa319a9
Stefan Smilko <smilko14@gmail.com>
2016-05-24 22:01:07 +0200

    MusicPlayer structure finished
    Part of GUI(structure) is finished

---
+++ MusicPlayer/src/main/java/controller/SceneController.java
@@ -0,0 +1,55 @@
+package controller;
+
+/**
+ * Created by Stefan Smiljkovic on 24.05.2016.
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class SceneController {
+
+    @FXML
+    private Button btPlay;
+
+    @FXML
+    private Button btPrev;
+
+    @FXML
+    private Button btNext;
+
+    @FXML
+    private Label labelYear;
+
+    @FXML
+    private Label labelTitle;
+
+    @FXML
+    private Label labelArtist;
+
+    @FXML
+    private Label labelAlbum;
+
+    @FXML
+    void progressClickHandler(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btPreviousHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btPlayHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btNExtHandler(ActionEvent event) {
+
+    }
+
+}
---
+++ MusicPlayer/src/main/java/model/MusicFinder.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by Stefan Smiljkovic on 24.05.2016.
+ */
+public class MusicFinder {
+
+    List<File> fileList;
+
+
+}
